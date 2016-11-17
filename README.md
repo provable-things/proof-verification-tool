0c2a687105b94f980c2292a0ad635f8b1aa13a19
D-Nice <yugoboy11@hotmail.com>
2016-11-17 12:37:37 -0500

    upgrade and cleanup

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # proof-verification-tool
 
+### Requirements
+
+Requires Node.js v4 or higher, not backwards compatible with v0.12 or earler. Tested working with the latest versions of 4, 5, 6 and 7.
+
 ### Install
 
     npm install
-  
+
 ### How to use
 
-Place proof files in the proof subdirectory (2 valid examples included)
+Place proof files in the proof subdirectory (2 valid examples included for testing, both should be passing verification)
 
-Then run with 
+Then run with
 
     node proofVerifier
-  
-And all valid proof files in proof folder will be verified, including computations
+
+Use -v option for more verbose logging
+
+Anyfiles in the proof folder will be verified, including computations if applicable
 
 The /lib/tlns/ subdirectory contains only umodified scripts from https://github.com/tlsnotary/pagesigner/tree/master/content for optimal integrity of the TLSN verification.
-
-###TODO
-Currently only does half of computation verification. Signature is not verified and checksum is not verified.
--- proofVerifier.js
+++ proofVerifier.js
@@ -1,66 +1,84 @@
-require('./lib/loadUtils.js');
-const https = require('https'),
-	fs = require('fs');
+'use strict';
 
-//load dependencies on-demand only
+checkVersion();
+
+require('./lib/loadUtils.js');
+
+const https = require('https'),
+	fs = require('fs'),
+	stdio = require('stdio');
+
+//load these dependencies on-demand only
 var tlsn,
 	comp;
 
+const ops = stdio.getopt({
+	'verbose': { key: 'v', description: 'Verbose logging' }
+});
+
+module.exports = { ops: ops };
+
+
+function checkVersion() {
+	if (process.version.substr(1, 1) === '0') {
+		console.log('Not compatible with version ' + process.version + ' of nodeJS, please use at least v4.x.x ');
+		console.log('exiting');
+		process.exit(1);
+	}
+}
+
 //fetches any files in proof folder and verifies them
 function autoVerify() {
-	var proofs = fs.readdirSync('./proof/');
-	//console.log(proofs);
+	const proofs = fs.readdirSync('./proof/');
+
+	if (proofs.length === 0) {
+		console.log('No files found in proof folder...');
+		process.exit(1);
+	}
+
+	if (ops.verbose)
+		console.log('\nFound proofs: \n' + proofs.join('\n') + '\n');
+
 	for (var i = 0; i < proofs.length; i++)
 		parseProofFile('./proof/' + proofs[i]);
 }
 
 function parseProofFile(proofFile) {
-	var parsedProof = new Uint8Array(fs.readFileSync(proofFile));
+	const parsedProof = new Uint8Array(fs.readFileSync(proofFile));
 	verifyProof(parsedProof, proofFile);
 }
 
 function verifyProof(data, file) {
-	type = getProofType(data);
+	const type = getProofType(data);
 	switch (type) {
 	case ('tlsn'):
 		try {
-			if (typeof (tlsn) === 'undefined')
+			if (typeof tlsn === 'undefined')
 				tlsn = require('./lib/tlsnVerify.js');
 
 			console.log('\n##############################################\n' + file.substr(file.lastIndexOf('/') + 1));
 			console.log('Verifying TLSNotary proof...')
-			var verificationResult = tlsn.verify(data);
+
+			const verificationResult = tlsn.verify(data);
 			console.log('TLSNotary proof successfully verified!');
-			var decryptedHtml = verificationResult[0];
-			//console.log(decryptedHtml);
+
+			const decryptedHtml = verificationResult[0];
+
 			if (isComputationProof(decryptedHtml)) {
-				if (typeof (comp) === 'undefined')
+				if (typeof comp === 'undefined')
 					comp = require('./lib/computationVerify.js');
 				console.log('Computation proof found...');
 
-				//second arg should take archive.zip
-				comp.verifyComputation(decryptedHtml, '');
+				comp.verifyComputation(decryptedHtml);
 				console.log('Computation verified!');
 			}
 		} catch (e) {
 			console.log(e);
 			return;
 		}
-		break; //placeholder
+		break;
+		//placeholder
 		/*case ('android'):
-			try {
-				data = (typeof (data) == 'object') ? strFromUtf8Ab(data) : data;
-				verifyProofandroid(data, extra[3], extra[4], extra[0], extra[1], extra[2]);
-			} catch (e) {
-				console.log('Verification error ', e);
-				extra[0][extra[1]].tx_count_1b = false;
-				extra[0][extra[1]].tx_count_1a = false;
-				extra[0][extra[1]].tx_count_1c = true;
-				postMessage({ type: 'textUpdate', value: ['android', false] });
-				postMessage({ type: 'chartUpdate', value: extra[0] });
-				checkProof(extra[2] + 1);
-				return;
-			}
 			break;*/
 	default:
 		console.log('Unknown proof type');
@@ -71,18 +89,18 @@ function verifyProof(data, file) {
 
 
 function isComputationProof(html) {
-	var compCheck1 = '</GetConsoleOutputResponse>';
+	const compCheck1 = '</GetConsoleOutputResponse>';
 	//Ensure GetConsoleOutputResponse is last element
-	var validator1 = html.indexOf(compCheck1) + compCheck1.length - html.length;
+	const validator1 = html.indexOf(compCheck1) + compCheck1.length - html.length;
 
-	var compCheck2 = 'Server: AmazonEC2';
-	var validator2 = html.indexOf(compCheck2);
+	const compCheck2 = 'Server: AmazonEC2';
+	const validator2 = html.indexOf(compCheck2);
 
 	return (validator1 === 0 && validator2 !== -1);
 }
 
 function getProofType(proof) {
-	var proofSlice = [
+	const proofSlice = [
 		{
 			"slice": 27,
 			"content": "tlsnotary notarization file",
@@ -96,7 +114,7 @@ function getProofType(proof) {
 	];
 
 	for (var i = 0; i < proofSlice.length; i++) {
-		var proofHeader = (typeof (proof) === 'object') ? ba2str(proof.slice(0, proofSlice[i].slice)) : proof.slice(0, proofSlice[i].slice);
+		const proofHeader = (typeof proof === 'object') ? ba2str(proof.slice(0, proofSlice[i].slice)) : proof.slice(0, proofSlice[i].slice);
 		if (proofHeader === proofSlice[i].content) {
 			return proofSlice[i].proofName;
 		}
@@ -104,6 +122,3 @@ function getProofType(proof) {
 }
 
 autoVerify();
-//read_tls_proof('QmNpJ7ytEiYjX1Tf9h5ikG9kuVt2KdjEQRg31Z9sakDHd5');
-//parseProofFile('QmPWgqzdzLW5LjgrjyPi2JUUzSsqaYze3LHFGGTA7UYWFH');
-//get_ipfs_proof('https://ipfs.infura.io/ipfs/', 'QmNpJ7ytEiYjX1Tf9h5ikG9kuVt2KdjEQRg31Z9sakDHd5')
